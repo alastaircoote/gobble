075d43f0b647e53feddc73be113df84175151c5c
Bogdan Chadkin <trysound@yandex.ru>
2016-06-05 13:57:15 +0300

    Add tests for merge nodes

--- test/initialisation.js
+++ test/initialisation.js
@@ -1,10 +1,20 @@
 var assert = require( 'assert' );
+var sander = require( 'sander' );
 var gobble = require( '..' );
+var path = require( 'path' );
+var os = require( 'os' );
 
 gobble.cwd( __dirname );
 
 module.exports = function () {
 	describe( 'initialisation', function () {
+		beforeEach( function () {
+			return Promise.all([
+				sander.rimraf( '.gobble-build' ),
+				sander.rimraf( 'tmp' )
+			]);
+		});
+
 		it( 'errors on non-existent directories (#12)', function () {
 			assert.throws( function () {
 				gobble( 'sample/nope' );
@@ -21,6 +31,22 @@ module.exports = function () {
 			assert.throws( function () {
 				gobble([ 42 ]);
 			}, /could not process input/ );
+		});
+
+		it( 'should merge nodes', function () {
+			var foo = gobble( 'sample/foo' );
+			var bar = gobble( 'sample/bar' );
+			return gobble( [ foo, bar ] ).build({
+				dest: 'tmp/output'
+			}).then( function () {
+				assert.deepEqual( sander.lsrSync('tmp/output'), [
+					path.normalize( 'a/dir/a.md' ),
+					path.normalize( 'b/dir/b.md' ),
+					path.normalize( 'bar.md' ),
+					path.normalize( 'baz.md' ),
+					path.normalize( 'foo.md' )
+				]);
+			});
 		});
 	});
 };
